1a85f862d466866d5801af2313f13197e8884938
Buguin <wybuguin@hotmail.com>
2017-12-14 23:16:07 +0800

    删除内部定义的Yaml文件，增加usage函数,使用函数实现读取Yaml文件的所有子节点

--- YamlRead/Program.cs
+++ YamlRead/Program.cs
@@ -8,68 +8,63 @@ namespace YamlDotNet.Samples
 {
     public class LoadYamlStream
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            string file_path = "..\\..\\config.yml";
-            // Setup the input
-            var input = new StreamReader(file_path, Encoding.Default);
-
-            // Load the stream
-            var yaml = new YamlStream();
-            yaml.Load(input);
-
-            // Examine the stream
-            var mapping =
-                (YamlMappingNode)yaml.Documents[0].RootNode;
-
-            foreach (var entry in mapping.Children)
+            if (args.Length <= 1)
             {
-                Console.WriteLine(((YamlScalarNode)entry.Key).Value);
-            }
-
-            // List all the items
-            var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("items")];
-            foreach (YamlMappingNode item in items)
+                usage();
+            }else if (args.Length == 2 & args[0] =="-f")
             {
-                Console.WriteLine(
-                    item.Children.Keys.ToString()
-                );
+                readYaml(args[1]);
             }
-            Console.ReadKey();
+            else
+            {
+                usage();
+            }            
         }
-
-        private const string Document = @"---
-            receipt:    Oz-Ware Purchase Invoice
-            date:        2007-08-06
-            customer:
-                given:   Dorothy
-                family:  Gale
-
-            items:
-                - part_no:   A4786
-                  descrip:   Water Bucket (Filled)
-                  price:     1.47
-                  quantity:  4
-
-                - part_no:   E1628
-                  descrip:   High Heeled ""Ruby"" Slippers
-                  price:     100.27
-                  quantity:  1
-
-            bill-to:  &id001
-                street: |
-                        123 Tornado Alley
-                        Suite 16
-                city:   East Westville
-                state:  KS
-
-            ship-to:  *id001
-
-            specialDelivery:  >
-                Follow the Yellow Brick
-                Road to the Emerald City.
-                Pay no attention to the
-                man behind the curtain.
-...";
+        static void useHead()
+        {
+            Console.WriteLine("Blue Ocean Software Yaml Tool 1.0.0.0 版");
+            Console.WriteLine("Copyright (C) Blue Ocean Software Corporation.");
+            Console.WriteLine("All Rights Reserved.");
+            Console.WriteLine();
+        }
+        static void usage()
+        {
+            useHead();
+            Console.WriteLine("Usage: iasl [Options] [File]");
+            Console.WriteLine("YamlRead -h                  This help");
+            Console.WriteLine("YamlRead -f File_Path        Parsing Yaml file");
+            Console.WriteLine();
+        }
+        static void readYaml(string file_path)
+        {
+            useHead();
+            // string file_path = "..\\..\\config.yml";
+            // Setup the input
+            if (File.Exists(@file_path) == false)
+            {
+                Console.WriteLine("Warning[0001]:The Yaml file is not exist:{0}", file_path);
+            }
+            else
+            {
+                var input = new StreamReader(file_path, Encoding.Default);
+                // Load the stream
+                var yaml = new YamlStream();
+                yaml.Load(input);
+                // Examine the stream
+                var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+                foreach (var entry in mapping.Children)
+                {
+                    Console.WriteLine(((YamlScalarNode)entry.Key).Value);
+                }
+                // List all the items
+                //var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("items")];
+                //foreach (YamlMappingNode item in items)
+                //{
+                //    Console.WriteLine(item.Children.Keys.ToString());
+                //}
+            }                
+        }
     }
 }
